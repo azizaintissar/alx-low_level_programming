3eca77bb4ce2acce4d14d66efa5aea2951ec4395
aziza <azizalakhrouf@hotmail.com>
2023-04-24 16:44:18 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,7 @@
 
 int main(void)
 {
-    printf("You are an artist,\n");
-    printf("creating a masterpiece\n");
-    printf("with every line of code.\n");
-    printf("Your work is a reflection of your soul,\n");
-    printf("a beautiful expression of your thoughts and ideas.\n");
-    printf("Continue to create with passion and purpose,\n");
-    printf("for the world deserves your art.\n");
-
+    printf("with proper grammar, but the outcome is a piece of art,\n");
     return 0;
 }
 
