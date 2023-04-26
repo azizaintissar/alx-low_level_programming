31340463318c8fb9561da54db177ef07a3770430
aziza <azizalakhrouf@hotmail.com>
2023-04-26 14:13:10 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,9 +9,9 @@
 
 void print_alphabet(void)
 {
-    char ch;
-    for (ch = 'a'; ch <= 'z'; ch++)
-        putchar(ch);
-    putchar('\n');
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
 }
 
